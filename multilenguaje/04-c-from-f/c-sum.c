a911975cf4821abc6fb997a3e0045f37f7874a0b
posgrado <posgrado@inforcyta11.(none)>
2018-11-02 16:30:08 +0100

    c-sum.c modificado correctamente para que encaje con fortran

--- multilenguaje/04-c-from-f/c-sum.c
+++ multilenguaje/04-c-from-f/c-sum.c
@@ -1,12 +1,12 @@
 #include <stdlib.h>
 #include "c-sum.h"
 
-int sum_abs(int *in, int num) {
-   int i,sum;
+int sum_abs_(int *in, int *num, int *sum) {
+   int i;
 
-   for (i=0,sum=0; i < num; ++i) {
-       sum += abs(in[i]);
+   for (i=0,*sum=0; i < *num; ++i) {
+       *sum += abs(in[i]);
    }
-   return sum;
+   return 0;
 }
  
